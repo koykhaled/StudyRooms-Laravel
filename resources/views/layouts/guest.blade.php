2aa57840d901677f62147eb6cfc1d030b0d523d8
koykhaled <koykhaled@gmail.com>
2023-12-21 23:32:18 +0300

    secure assets

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,18 +7,18 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title')</title>
-    
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    
-    <!-- Scripts -->    
+
+    <!-- Scripts -->
     <link rel="shortcut icon" href="{{asset('assets/favicon.ico')}}" type="image/x-icon" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}" />
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-    
+
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <div>
             <a href="/">
