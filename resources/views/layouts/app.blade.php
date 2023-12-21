7f683253774b67ed6ef0268ef1dbb9317cceb878
koykhaled <koykhaled@gmail.com>
2023-12-21 23:19:36 +0300

    add https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="{{secure_asset('assets/favicon.ico')}}" type="image/x-icon" />
-    <link rel="stylesheet" href="{{ secure_asset('css/dashboard/style.css') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon" />
+    <link rel="stylesheet" href="{{ asset('css/dashboard/style.css') }}" />
     <title>@yield('title')</title>
 </head>
 
@@ -25,10 +25,10 @@
 
 
 
-    <script src="{{asset('js/script.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script src="{{asset('js/dashboard/main.js')}}"></script>
+    <script src="{{ asset('js/dashboard/main.js') }}"></script>
     @notifyJs
 </body>
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,11 +4,10 @@
 <head>
     @notifyCss
     <meta charset="UTF-8" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="{{ secure_asset('assets/favicon.ico') }}" type="image/x-icon" />
-    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <title>StudyBuddy - Find study partners around the world!</title>
 </head>
 
