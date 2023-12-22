5bb9a09718a65fd741fb00d8fd90aaa6646afd2d
koykhaled <koykhaled@gmail.com>
2023-12-22 04:50:31 +0300

    fixing some errors

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
-use App\Http\Trait\UploadImageTrait;
 use App\Models\Message;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
--- app/Http/Controllers/UploadImageTrait.php
+++ app/Http/Controllers/UploadImageTrait.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Http\Trait;
+namespace App\Http\Controllers;
 
 trait UploadImageTrait
 {
